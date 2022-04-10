c7693066372411189fb2bcab2dbca611dbe26c1a
Mihail <mr.derks1989@gmail.com>
2022-04-10 16:16:28 +0300

    react first integration

--- server/server/server.js
+++ server/server/server.js
@@ -8,9 +8,10 @@ const cors = require('cors');
 require('dotenv').config();
 
 // const PORT = process.env.PORT || 3007;
-const PORT = 4001;
+const PORT = 4042;
 const app = express();
 
+const pathRoot = require('path').join(__dirname, 'client', 'build');
 const indexRouter = require('./routes/index');
 const place = require('./routes/place');
 const event = require('./routes/event');
@@ -28,7 +29,8 @@ app.use(cors(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(process.env.PWD, 'public')));
+// app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -41,10 +43,25 @@ app.use(session({
 app.get('/test', (req, res) => {
   res.json({ message: 'ok' });
 });
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/user', user);
 app.use('/places', place);
 app.use('/events', event);
+
+// app.use(express.static(pathRoot));
+// app.get('/*', (req, res) => {
+//   res.sendFile('index.html', { root: pathRoot });
+// });
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/index.html`));
+// });
+
+app.get('/*', (req, res) => {
+  // res.sendFile(path.join((__dirname, 'build', 'index.html')));
+  res.sendFile('./build/index.html', { root: __dirname });
+});
+
 // check
 app.listen(PORT, () => {
   console.log('Port is ok');
