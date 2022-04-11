4eab65e573d4bade0f98c2b43d9433c714211145
Михаил Журавлев <mihailzuravlev@MacBook-Air-Mihail.local>
2022-04-11 18:24:16 +0300

    signUp_signIn_eventControl_backend

---
+++ server/routes/event.router.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const eventController = require('../controllers/event.controller');
+const checkAuth = require('../middlewares/checkAuth')
+const checkAuthor = require('../middlewares/checkAuthor');
+
+const eventRouter = Router();
+
+eventRouter.get('/', checkAuth, eventController.getAllUsers);
+eventRouter.route('/:id')//айди события поидее если не будет работать то  поменять на userId
+  .patch(checkAuth, checkAuthor, eventController.editEvent)
+  .get(checkAuth, eventController.getEvent);
+
+module.exports = eventRouter;
